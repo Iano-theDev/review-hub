fd573d7e7134c20d0519475950acb4bc12a36b88
Iano-theDev <sinceianmike@gmail.com>
2023-04-08 21:55:51 +0300

    Update: add register form

--- reviewHub/src/app/features/auth/sign-up/sign-up.component.ts
+++ reviewHub/src/app/features/auth/sign-up/sign-up.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+
+  onSubmit(form:NgForm){
+    alert(`Welcome ${form.value.username}. We're glad to have you here!`)
+  }
 
 }
