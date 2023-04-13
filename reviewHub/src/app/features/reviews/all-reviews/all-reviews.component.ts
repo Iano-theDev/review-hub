ed8de7fb17001e6eb0654c91c158639a53ad4c44
Iano-theDev <sinceianmike@gmail.com>
2023-04-13 23:44:31 +0300

    Update reviews: review components review model

---
+++ reviewHub/src/app/features/reviews/all-reviews/all-reviews.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-all-reviews',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './all-reviews.component.html',
+  styleUrls: ['./all-reviews.component.css']
+})
+export class AllReviewsComponent {
+
+}
---
+++ reviewHub/src/app/models/review.model.ts
@@ -0,0 +1,11 @@
+export interface Review{
+    reviewId: string,
+    reviewItemId: string,
+    userId: string,
+    category: string,
+    rating: number,
+    title: string,
+    body: string,
+    vote: number,
+    createdAt: string,
+}
