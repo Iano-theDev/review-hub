7ded2b866b72c85a28f2333ad0c0b2952ddee353
Iano-theDev <sinceianmike@gmail.com>
2023-05-04 23:34:56 +0300

    remove unused currency pipe

--- reviewHub/src/app/shared/pipes/usd-ksh.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UsdKshPipe } from './usd-ksh.pipe';
-
-describe('UsdKshPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UsdKshPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
